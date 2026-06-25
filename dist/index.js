2944b3196225d8b6a894cf09384413ca31c5870e
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-25 22:38:48 +0530

    Removed dist folder from tracking

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `Hello ${person} ! Welcome to TypeScript Learning `;
-}
-const Username = "TypeScript 😎";
-console.log(greet(Username));
-//# sourceMappingURL=index.js.map
