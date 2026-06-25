bc0912d0ed957bab4449395638bf52fea3dc5fb7
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-25 23:12:52 +0530

    Understand type Narrowing .

---
+++ src/typeNarrowing.ts
@@ -0,0 +1,65 @@
+function getChai(kind: string | number) {
+  if (typeof kind === "string") {
+    return `Making Chai ${kind} ... ->`;
+  }
+  return `Your Order id: ${kind}.`;
+}
+
+function ServeChai(msg?: string) {
+  if (msg) {
+    return `Serving Your ${msg}`;
+  }
+  return `Serving Default Masala Chai.`;
+}
+
+function OrderChai(size: "small" | "Medium" | "Large" | Number) {
+  if (size === "small") {
+    return `Small Chai.`;
+  } else if (size === "Medium") {
+    return `Medium Chai.`;
+  } else if (size === "Large") {
+    return "Large Chai.";
+  } else {
+    return `Order Number id: ${size}`;
+  }
+}
+
+class KulhadChai {
+  serve() {
+    return `Serving Kulhad Chai .`;
+  }
+}
+
+class CuttingChai {
+  serve() {
+    return `Serving Cutting Chai .`;
+  }
+}
+
+function serve(chai: KulhadChai | CuttingChai) {
+  if (chai instanceof KulhadChai) {
+    return chai.serve();
+  }
+}
+
+type ChaiOrder = {
+  type: string;
+  sugar: number;
+};
+
+function isChaiOrder(obj: any): obj is ChaiOrder {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    typeof obj.type === "string" &&
+    typeof obj.sugar === "number"
+  );
+}
+
+
+function serveOrder(item:ChaiOrder | string){
+    if(isChaiOrder(item)){
+       return `Serving ${item.type} chai with ${item.sugar} sugar ` 
+    }
+    return `Serving custom chai : ${item}`
+}
