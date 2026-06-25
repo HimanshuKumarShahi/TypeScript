c46594b227558e7354094743feb6fd826128ab2c
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-25 21:06:40 +0530

    See the types similar to js

---
+++ dist/types.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let drink = "chai";
+drink = "masala";
+console.log(drink);
+let num = Math.random() > 0.5 ? 10 : 5;
+console.log(num);
+let cups = Math.random() > 0.5 ? 10 : "5";
+console.log(cups);
+let book = "Ramayan";
+book = "HC verma";
+console.log(book);
+//# sourceMappingURL=types.js.map
---
+++ src/types.ts
@@ -0,0 +1,18 @@
+let drink = "chai";
+drink = "masala";
+
+console.log(drink);
+
+let num = Math.random() > 0.5 ? 10 : 5;
+
+console.log(num);
+
+let cups = Math.random() > 0.5 ? 10 : "5";
+
+console.log(cups);
+
+let book: string = "Ramayan";
+
+book = "HC verma";
+
+console.log(book);
