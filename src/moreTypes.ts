55c11efef97e372432b758ccbfd8b4877dbad1bc
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-26 18:54:29 +0530

    Void or never working in Ts

--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -1,34 +1,55 @@
-let response:any = "74"
+let response: any = "74";
 
-let numericLength:number = (response as string).length
+let numericLength: number = (response as string).length;
 
-
-type Book={
-    name:string
-}
+type Book = {
+  name: string;
+};
 
 let bookString = '{"name":"Little boy"}';
 // let bookObject = JSON.parse(bookString)
-let bookObject = JSON.parse(bookString) as string
+let bookObject = JSON.parse(bookString) as string;
 
 console.log(bookObject);
 
+const InputUserData = document.getElementById("username") as HTMLInputElement;
 
-const InputUserData = document.getElementById("username") as HTMLInputElement
+let value: any;
 
-let value:any
+value = "Chai";
+value = ["1", "5", "9"];
+value = 2.9;
+value.toUpperCase();
 
-value="Chai"
-value=['1','5','9']
-value=2.9
-value.toUpperCase()
+let NewValue: unknown;
 
-let NewValue:unknown
-
-NewValue="Chai"
-NewValue=['1','5','9']
-NewValue=2.9
-if(typeof NewValue === 'string'){
-    NewValue.toUpperCase()
+NewValue = "Chai";
+NewValue = ["1", "5", "9"];
+NewValue = 2.9;
+if (typeof NewValue === "string") {
+  NewValue.toUpperCase();
 }
 
+try {
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  }
+  console.log("Error", error);
+}
+
+const data: unknown = "Chai Aur Code ";
+const StrData: string = data as string;
+
+type Role = "admin" | "user" | "SuperAdmin";
+
+function redirectBasedOnRole(role: Role): void {
+  if (role === "admin") {
+    console.log("Redirecting to admin dashboard. ");
+    return;
+  } else if (role === "user") {
+    console.log("Redirecting to user dashboard.");
+    return;
+  }
+  role;
+}
