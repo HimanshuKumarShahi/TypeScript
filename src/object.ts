4093ac615e3afa0461b7f2456b64f91a7170967a
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-06-26 20:34:30 +0530

    Work with multiple type in same class.

---
+++ src/object.ts
@@ -0,0 +1,84 @@
+/**
+ * {
+ * name:string
+ * price:number
+ * isHot:boolean
+ * }
+ */
+const chai = {
+  name: "Masala Chai",
+  price: 20,
+  isHot: true,
+};
+
+let tea: {
+  name: string;
+  price: number;
+  isHot: boolean;
+};
+
+tea = {
+  name: "Lemon",
+  price: 49,
+  isHot: true,
+};
+
+type Tea = {
+  name: string;
+  Price: number;
+  ingredients: string[];
+  isOff: boolean;
+};
+
+const FeaturedChai: Tea = {
+  name: "Ginger Chai",
+  Price: 30,
+  ingredients: ["ginger", "Tulsi", "Tea leaves"],
+  isOff: true,
+};
+
+type Cup = { size: string };
+
+let smallCup: Cup = {
+  size: "120 ml",
+};
+
+let bigCup = {
+  size: "520 ml",
+  material: "steel",
+};
+
+console.log(smallCup === bigCup);
+
+type Brew = { brewTime: number };
+const coffee = {
+  brewTime: 3,
+  beans: "Arabica",
+};
+const ChaiBrew: Brew = coffee;
+
+type User = {
+  username: string;
+  password: string;
+};
+
+const u1: User = {
+  username: "Himanshu",
+  password: "123",
+};
+
+type Item = {
+  name: string;
+  quantity: number;
+};
+
+type Address = {
+  street: string;
+  pin: number;
+};
+
+type Order = {
+  id: string;
+  items: Item[];
+  address: Address;
+};
