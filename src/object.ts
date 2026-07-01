8e8e3047528ab4f1ac04c24ad7715e0b6f8d764e
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-07-01 21:00:46 +0530

    Understand Required , omit , partial in TS

--- src/object.ts
+++ src/object.ts
@@ -50,9 +50,57 @@ let bigCup = {
 
 console.log(smallCup === bigCup);
 
-type Brew = {brewTime:number}
+type Brew = { brewTime: number };
 const coffee = {
-    brewTime:3,
-    beans:"Arabica"
-}
-const ChaiBrew:Brew = coffee
+  brewTime: 3,
+  beans: "Arabica",
+};
+const ChaiBrew: Brew = coffee;
+
+type User = {
+  username: string;
+  password: string;
+};
+
+const u: User = {
+  username: "Boss",
+  password: "123",
+};
+
+type Item = { name: string; quantity: number };
+type Address = { Street: string; pin: number };
+
+type Order = {
+  id: string;
+  item: Item[];
+  address: Address;
+};
+
+type Product = {
+  name: string;
+  price: number;
+  isHot: boolean;
+};
+
+const UpdateProduct = (updates: Partial<Product>) => {
+  console.log("Updating Chai With ", updates);
+};
+
+UpdateProduct({ price: 200 });
+UpdateProduct({ isHot: false });
+UpdateProduct({});
+
+type Buygoods = {
+  name?: string;
+  quantity?: number;
+};
+
+const BuyingProduct = (Buygoods: Required<Buygoods>) => {
+  console.log(BuyingProduct);
+};
+
+BuyingProduct({
+  name: "Books",
+  quantity: 5,
+});
+
